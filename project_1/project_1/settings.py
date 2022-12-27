300b3d89be069e16a9725d9fd23618f052481e1b
S Rosenberg <sbastian.rosenberg@gmail.com>
2022-12-27 17:36:02 -0300

    project_1/settings.py fix exposed secret key

--- project_1/project_1/settings.py
+++ project_1/project_1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-##=uexou7s$_w+qs6-9msajun0gsg6y^yr6c((eal(k@woi)lp'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
