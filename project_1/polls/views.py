7450f57c1723fc592758eee0b7271c4114c406d5
S Rosenberg <sbastian.rosenberg@gmail.com>
2022-12-29 16:40:47 -0300

    polls/views.py some generic functions & filter only questions with choices

--- project_1/polls/views.py
+++ project_1/polls/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.utils import timezone
 from django.urls import reverse
 from django.views import generic
+from django.db.models.query import QuerySet
 from .models import Question, Choice
 
 ##################### VIEWS DEFINIDOS COMO FUNCIONES ##############################
@@ -47,28 +48,44 @@ def results(request, question_id:int) -> HttpResponse:
 ##################### VIEWS DEFINIDOS COMO CLASES ##############################
 ############################# GENERICAS ########################################
 
+def get_queryset_older_than_now() -> QuerySet[Question]:
+    return Question.objects.filter(pub_date__lte=timezone.now())
+
+def get_questions_with_choices(query_set:QuerySet[Question]):
+    return [question for question in query_set if question.choice_set.all()]
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.filter(
-            pub_date__lte=timezone.now() # filtros: 
-            ).order_by('-pub_date')[:5]
+        query_set = get_queryset_older_than_now().order_by('-pub_date')[:5]
+        return get_questions_with_choices(query_set)
 
 # las vistas genericas necesitan saber sobre que modelo actuaran (para eso se usa el attr model)
 # DetailView espera que la primary key este en la URL -> por eso se cambia de question_id a pk en polls/urls.py
 # se crea el attr template_name ya que por default es <app name>/<model name>_detail.html
 # generic views doc: https://docs.djangoproject.com/en/4.1/topics/class-based-views/
 
-class DetailView(generic.DeleteView):
+class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excluye Question no publicada aun
+        """
+        return get_queryset_older_than_now()
     
-class ResultsView(generic.DeleteView):
+class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+    def get_queryset(self):
+        """
+        Excluye Question no publicada aun
+        """
+        return get_queryset_older_than_now()
 
 def vote(request, question_id:int) -> HttpResponse:
     question = get_object_or_404(Question, pk=question_id)
