2dfd49bb022ad469117021d896a275c403540d9b
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-01-08 20:25:06 -0300

    creating models for login & passwords apps

--- password_manager/login/models.py
+++ password_manager/login/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=40, unique=True)
+    password = models.CharField(max_length=40)
+
+    def __str__(self) -> str:
+        return f'User({self.username})'
--- password_manager/passwords/models.py
+++ password_manager/passwords/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from password_manager.login.models import User
 
-# Create your models here.
+class Site(models.Model):
+    site_name = models.CharField(max_length=100)
+    site_url = models.CharField(max_length=100, primary_key=True)
+
+class SavedPassword(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
