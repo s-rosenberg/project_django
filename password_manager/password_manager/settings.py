debdf25318ae40634b73df0417405804ff229bdd
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-01-08 19:35:08 -0300

    password_manager/settings.py delete secrets again xd

--- password_manager/password_manager/settings.py
+++ password_manager/password_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xd!%4#c772b^*cp5-^q))to6lg5-q@^opo*ij9i(x!9wkopx+c'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
