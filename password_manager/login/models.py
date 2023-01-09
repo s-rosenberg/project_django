2fcaaf9e2de990441b739bf0135e09beae1b0513
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-01-09 18:56:11 -0300

    delete login app (not needed)

--- password_manager/login/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=40, unique=True)
-    password = models.CharField(max_length=40)
-
-    def __str__(self) -> str:
-        return f'User({self.username})'
