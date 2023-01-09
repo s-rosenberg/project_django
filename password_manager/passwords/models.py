b36b7c35340a87d124613b9f896298514920c419
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-01-09 18:55:22 -0300

    password_manager/passwords/models.py change user from login to django User model

--- password_manager/passwords/models.py
+++ password_manager/passwords/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from password_manager.login.models import User
+# from password_manager.login.models import User
+from django.contrib.auth.models import User
 
 class Site(models.Model):
     site_name = models.CharField(max_length=100)
@@ -8,6 +9,8 @@ class Site(models.Model):
 class SavedPassword(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    site = models.ForeignKey(Site, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE) # esto ta mal creo
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # idem
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
