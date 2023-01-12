9336aaba2955270f58945d5b1107215b477949a0
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-01-11 21:04:51 -0300

    passwords/views.py add save password view

--- password_manager/passwords/views.py
+++ password_manager/passwords/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from .forms import PasswordForm
 
-# Create your views here.
+def save_password(request:HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = PasswordForm(request.POST)
+        if form.is_valid():
+            return HttpResponse('PIOLA')
+    else: 
+        form = PasswordForm()
+    return render(request, 'passwords/save_password.html', {'form':form})
+
+# def new_password(request):
+#     form = PasswordForm()
+#     return render(request, template_name='passwords/save_password.html')
