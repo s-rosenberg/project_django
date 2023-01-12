58b7cd8add7e31d6765aa509bd59d9e94754b208
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-01-12 19:16:23 -0300

    passwords/views.py logic to save password

--- password_manager/passwords/views.py
+++ password_manager/passwords/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from .forms import PasswordForm
+from .models import SavedPassword, Site
 
 def save_password(request:HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = PasswordForm(request.POST)
         if form.is_valid():
+            user = form.cleaned_data['user']
+            password = form.cleaned_data['password']
+            site_name = form.cleaned_data['site']
+            site_url = form.cleaned_data['site_url']
+            site = Site(site_name=site_name, site_url=site_url)
+            saved_password = SavedPassword(
+                username=user, 
+                password=password,
+                site=site
+                )
+            site.save()
+            saved_password.save()
             return HttpResponse('PIOLA')
     else: 
         form = PasswordForm()
