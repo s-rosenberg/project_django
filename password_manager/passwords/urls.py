4fed2fad538a4fc24e554c1a6ca359474cbddf85
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-01-12 19:16:00 -0300

    password_manager/passwords/urls.py delete unesed url

--- password_manager/passwords/urls.py
+++ password_manager/passwords/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 app_name = 'passwords'
 urlpatterns = [
-    # path('new_password/', views.new_password, name='new_password'),
     path('save_password/', views.save_password, name='save_password')
 ]
