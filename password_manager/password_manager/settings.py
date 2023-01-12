3b465b0f636280625b48a7a2c1acb57b211c8500
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-01-11 21:02:24 -0300

    settings.py add template DIRS

--- password_manager/password_manager/settings.py
+++ password_manager/password_manager/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'password_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
