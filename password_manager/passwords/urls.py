db4a390ed372a15362669ab1f79ab8bc381bc5fe
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-01-16 22:57:42 -0300

    password_manager/passwords/urls.py add default login

--- password_manager/passwords/urls.py
+++ password_manager/passwords/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 app_name = 'passwords'
 urlpatterns = [
-    path('save_password/', views.save_password, name='save_password')
+    path('save_password/', views.save_password, name='save_password'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
