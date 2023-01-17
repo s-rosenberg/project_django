7648356b679426fab390b4d140b86291d41163bf
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-01-16 22:55:47 -0300

    password_manager/settings.py add login rediretc

--- password_manager/password_manager/settings.py
+++ password_manager/password_manager/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# redirect when logged in
+LOGIN_REDIRECT_URL = '/save_password'
