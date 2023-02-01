0c9a3ad810ae1ce8d223b13de84a1e5817fdbb37
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-01-31 22:21:46 -0300

    moving password manager to another repository

--- password_manager/passwords/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class PasswordForm(forms.Form):
-    user = forms.CharField(label='User', max_length=100)
-    password = forms.CharField(label='Passowrd', max_length=100, widget=forms.PasswordInput())
-    site = forms.CharField(label='Sitename', max_length=100)
-    site_url = forms.CharField(label='Site URL', max_length=100)
